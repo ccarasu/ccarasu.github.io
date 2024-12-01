803f8390bbb61bf30b8d9250523c712bb893aeb3
전승민 <seungmin802@gmail.com>
2024-12-01 20:39:59 +0900

    햄버거 메뉴 회왼가입과 로그인 페이지에서도 기능하도록 코드 추가

--- public/join/join.js
+++ public/join/join.js
@@ -1,4 +1,15 @@
 $(function () {
+    // 햄버거 메뉴 클릭 시 메뉴 열기/닫기
+    $(".mobile_menu").click(function () {
+        $(".nav-menu").toggleClass("open"); // nav-menu에 'open' 클래스를 토글하여 메뉴 열고 닫기
+        $("body").css("overflow", $(".nav-menu").hasClass("open") ? "hidden" : "auto");
+    });
+
+    // X 버튼 클릭 시 메뉴 닫기
+    $("#closeMenu").click(function () {
+        $(".nav-menu").removeClass("open"); // nav-menu에서 'open' 클래스를 제거하여 메뉴 닫기
+        $("body").css("overflow", "auto"); // 메뉴가 닫히면 body의 overflow를 'auto'로 복원하여 스크롤이 가능하도록 함
+    });
     // 회원가입 버튼 클릭 이벤트
     $(".signup-button").click(function (event) {
         event.preventDefault(); // 기본 동작 방지
