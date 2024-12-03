774ddc7af9c4276553b553e5434d1b91e92af9c4
전승민 <seungmin802@gmail.com>
2024-12-03 20:39:20 +0900

    .

--- public/js/RDNum.js
+++ public/js/RDNum.js
@@ -24,7 +24,18 @@ let isLoading = false; // 로딩 상태
 let allDrawsLoaded = false; // 모든 데이터 로드 여부
 
 $(function () {
-  // 로또 번호 생성 (변경 없음)
+  // 햄버거 메뉴 클릭 시 메뉴 열기/닫기
+  $(".mobile_menu").click(function () {
+    $(".nav-menu").toggleClass("open"); // nav-menu에 'open' 클래스를 토글하여 메뉴 열고 닫기
+    $("body").css("overflow", $(".nav-menu").hasClass("open") ? "hidden" : "auto");
+  });
+
+  // X 버튼 클릭 시 메뉴 닫기
+  $("#closeMenu").click(function () {
+      $(".nav-menu").removeClass("open"); // nav-menu에서 'open' 클래스를 제거하여 메뉴 닫기
+      $("body").css("overflow", "auto"); // 메뉴가 닫히면 body의 overflow를 'auto'로 복원하여 스크롤이 가능하도록 함
+  });
+  // 로또 번호 생성
   $("#CreateNumber").click(function () {
     const lotto_numbers = document.getElementById('lotto-numbers');
     lotto_numbers.innerHTML = '';
